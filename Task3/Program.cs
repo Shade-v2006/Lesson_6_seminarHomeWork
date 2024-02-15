779d40d8e0143de18f6b70fb1895e5b2c77a4dfb
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-15 23:02:09 +0700

    проверка на палиндром

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+bool Palindrom(string symbol)
+{
+    for (int i = 0; i < symbol.Length / 2; i++)
+
+        if (symbol[i] != symbol[symbol.Length - i - 1])
+            return false;
+    return true;
+}
+string symbol;
+Console.WriteLine("Проверка на палиндром\nВведите строку:");
+symbol = Console.ReadLine()!;
+if (Palindrom(symbol))
+    Console.WriteLine("Эта строка - палиндром");
+else Console.WriteLine("Эта строка - не палиндром");
