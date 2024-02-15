e6a065955332a3346ca73b3f92ce279b7766c4c4
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-15 22:50:24 +0700

    изменение регистра букв

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+    Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+    Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+*/
+string input = "aAFcDFoPErN";
+System.Console.WriteLine(input);
+
+string result = input.ToLower();
+System.Console.WriteLine(result);
