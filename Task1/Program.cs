e7bbec131340a997865bd418db00055b2e5ec760
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-15 20:38:23 +0700

    из двумерного массива в строку

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,3 +10,24 @@
 Сформировать строку, в которой слова расположены в обратном порядке. В полученной 
 строке слова должны быть также разделены пробелами.
 */
+char[,] chars = new char[,] {{'H', 'e', 'l', 'l', 'o'},
+                             {'W', 'o', 'r', 'l', 'd'}}; // Создаем массив символов
+
+string makeString(char[,] chars) // Функция передает массив символов и преобразует их в строку
+{
+    string sim = ""; // Переменная принимающаа символы и превращающая их в строку
+    for (int i = 0; i < chars.GetLength(dimension: 0); i++)
+    {
+        for (int j = 0; j < chars.GetLength(dimension: 1); j++)
+        {
+            if (i == 1 && j == 0)
+            {
+                sim += " ";
+            }
+            sim += chars[i, j];
+        }
+    }
+    return sim;
+}
+string sim = makeString(chars); // 
+System.Console.WriteLine(value: sim); // Функция выводит строку на печать
